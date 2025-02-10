6af76cfa9d352ce0f32626488fed9d440270fd62
Akshat Patel <akshat.patel16@gmail.com>
2025-02-10 17:08:05 +0530

    Add watchlist app

---
+++ WatchlistApp/WatchlistApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchlistApp
+//
+//  Created by Akshat Patel on 10/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchlistApp/WatchlistApp/WatchlistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistAppApp.swift
+//  WatchlistApp
+//
+//  Created by Akshat Patel on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
